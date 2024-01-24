9f10af8932154574f904faed27e8d12e88bec909
LuisFernandoLG <luislopez1099lg@gmail.com>
2024-01-24 13:24:29 -0700

    Refactor API routes and add info endpoint

--- routes/apiV1.php
+++ routes/apiV1.php
@@ -6,25 +6,25 @@ use App\Http\Controllers\KingdomController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::get('/kingdoms', [KingdomController::class, 'index']);
-Route::get('/kingdoms/paginated', [KingdomController::class, 'paginated']);
-Route::get('/kingdoms/slug/{slug}', [KingdomController::class, 'showBySlug']);
-Route::get('/kingdoms/{id}', [KingdomController::class, 'show']);
+Route::get('/kingdoms', [KingdomController::class, 'index'])->name('kingdoms.index');
+Route::get('/kingdoms/paginated', [KingdomController::class, 'paginated'])->name('kingdoms.paginated');
+Route::get('/kingdoms/slug/{slug}', [KingdomController::class, 'showBySlug'])->name('kingdoms.showBySlug');
+Route::get('/kingdoms/{id}', [KingdomController::class, 'show'])->name('kingdoms.show');
 
 
-Route::get('/episodes', [EpisodeController::class, 'index']);
-Route::get('/episodes/paginated', [EpisodeController::class, 'paginated']);
-Route::get('/episodes/slug/{slug}', [EpisodeController::class, 'showBySlug']);
-Route::get('/episodes/{id}', [EpisodeController::class, 'show']);
+Route::get('/episodes', [EpisodeController::class, 'index'])->name('episodes.index');
+Route::get('/episodes/paginated', [EpisodeController::class, 'paginated'])->name('episodes.paginated');
+Route::get('/episodes/slug/{slug}', [EpisodeController::class, 'showBySlug'])->name('episodes.showBySlug');
+Route::get('/episodes/{id}', [EpisodeController::class, 'show'])->name('episodes.show');
 
-Route::get('/characters', [CharacterController::class, 'index']);
-Route::get('/characters/paginated', [CharacterController::class, 'paginated']);
-Route::get('/characters/slug/{slug}', [CharacterController::class, 'showBySlug']);
-Route::get('/characters/{id}', [CharacterController::class, 'show']);
+Route::get('/characters', [CharacterController::class, 'index'])->name('characters.index');
+Route::get('/characters/paginated', [CharacterController::class, 'paginated'])->name('characters.paginated');
+Route::get('/characters/slug/{slug}', [CharacterController::class, 'showBySlug'])->name('characters.showBySlug');
+Route::get('/characters/{id}', [CharacterController::class, 'show'])->name('characters.show');
 
 Route::fallback(function (){
     return response()->json([
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,43 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-})->name('login');;
+    return redirect(route('info'));
+})->name('home');
+
+Route::get('/info', function () {
+    return response()->json([
+        'app' => env('APP_NAME'),
+        'version' => env('APP_VERSION'),
+        'author' => env('APP_AUTHOR'),
+        'github' => env('APP_GITHUB_URL'),
+        'documentation' => env('APP_DOCUMENTATION_URL'),
+        'endpoints' => [
+            'kingdoms' => [
+                'list' => route('kingdoms.index'),
+                'paginated' => route('kingdoms.paginated'),
+                'show' => route('kingdoms.show', ['id' => 1]),
+                'showBySlug' => route('kingdoms.showBySlug', ['slug' => 'kingdom-slug']),
+            ],
+            'episodes' => [
+                'list' => route('episodes.index'),
+                'paginated' => route('episodes.paginated'),
+                'show' => route('episodes.show', ['id' => 1]),
+                'showBySlug' => route('episodes.showBySlug', ['slug' => 'episode-slug']),
+            ],
+            'characters' => [
+                'list' => route('characters.index'),
+                'paginated' => route('characters.paginated'),
+                'show' => route('characters.show', ['id' => 1]),
+                'showBySlug' => route('characters.showBySlug', ['slug' => 'character-slug']),
+            ],
+        ]
+    ]);
+})->name('info');
+
+Route::fallback(function (){
+    return response()->json([
+        'message' => 'Endpoint not found',
+        'documentation' => env('APP_DOCUMENTATION_URL'),
+        'base_url' => env('APP_URL'),
+    ]);
+});
