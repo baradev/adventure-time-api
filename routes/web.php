9b753eb2f00d52e38b3fff5bd5c5eb3deeab1520
LuisFernandoLG <luislopez1099lg@gmail.com>
2024-02-10 13:54:12 -0700

    fix:

--- routes/web.php
+++ routes/web.php
@@ -17,44 +17,6 @@ Route::get('/', function () {
     return redirect(route('info'));
 })->name('home');
 
-Route::get('/info', function () {
-    return response()->json([
-        'app' => env('APP_NAME'),
-        'version' => env('APP_VERSION'),
-        'author' => env('APP_AUTHOR'),
-        'github' => env('APP_GITHUB_URL'),
-        'documentation' => env('APP_DOCUMENTATION_URL'),
-        'data_count' => [
-            'kingdoms' => \App\Models\Kingdom::count(),
-            'episodes' => \App\Models\Episode::count(),
-            'characters' => \App\Models\Character::count(),
-        ],
-        'endpoints' => [
-            'kingdoms' => [
-                'list' => route('kingdoms.index'),
-                'paginated' => route('kingdoms.paginated'),
-                'show' => route('kingdoms.show', ['ids' => 1]),
-                'showBySlug' => route('kingdoms.showBySlug', ['slug' => 'kingdom-slug']),
-            ],
-            'episodes' => [
-                'list' => route('episodes.index'),
-                'paginated' => route('episodes.paginated'),
-                'show' => route('episodes.show', ['ids' => 1]),
-                'showBySlug' => route('episodes.showBySlug', ['slug' => 'episode-slug']),
-            ],
-            'characters' => [
-                'list' => route('characters.index'),
-                'paginated' => route('characters.paginated'),
-                'show' => route('characters.show', ['ids' => 1]),
-                'showBySlug' => route('characters.showBySlug', ['slug' => 'character-slug']),
-            ],
-        ],
-        "info_taken_from" =>[
-            "https://adventuretime.fandom.com/wiki/Adventure_Time_Wiki",
-            "https://freepngimg.com/cartoon/adventure-time"
-        ]
-    ]);
-})->name('info');
 
 Route::fallback(function (){
     return response()->json([
